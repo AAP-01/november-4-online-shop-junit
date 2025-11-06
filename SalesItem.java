67f020f3fb9de8316bd5a43f910627bbb09ec6fa
John Benedict Castroverde <jbthememer43@gmail.com>
2025-11-05 23:19:31 -0500

    step 21

--- SalesItem.java
+++ SalesItem.java
@@ -117,9 +117,11 @@ public class SalesItem
         System.out.println("Price: " + priceString(price));
         System.out.println();
         System.out.println("Customer comments:");
-        for(Comment comment : comments) {
+        int i = 0;
+        while(i < comments.size()) {
             System.out.println("-------------------------------------------");
-            System.out.println(comment.getFullDetails());
+            System.out.println(comments.get(i).getFullDetails());
+            i++;
         }
         System.out.println();
         System.out.println("===========================================");
@@ -132,13 +134,14 @@ public class SalesItem
      */
     public Comment findMostHelpfulComment()
     {
-        Iterator<Comment> it = comments.iterator();
-        Comment best = it.next();
-        while(it.hasNext()) {
-            Comment current = it.next();
+        int i = 1;
+        Comment best = comments.get(0);
+        while(i < comments.size()) {
+            Comment current = comments.get(i);
             if(current.getVoteCount() > best.getVoteCount()) {
                 best = current;
             }
+            i++;
         }
         return best;
     }
@@ -158,9 +161,11 @@ public class SalesItem
      */
     private Comment findCommentByAuthor(String author)
     {
-        for(Comment comment : comments) {
-            if(comment.getAuthor().equals(author)) {
-                return comment;
+        int i = 0;
+        while(i < comments.size()) {
+            if(comments.get(i).getAuthor().equals(author)) {
+                i++;
+                return comments.get(i);
             }
         }
         return null;
